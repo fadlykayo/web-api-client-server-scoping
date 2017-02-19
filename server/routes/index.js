4e0ee159a2e48a1a0312e851a884397c70dd2aa6
Fadly Kayo <fadly.kayo@googlemail.com>
2017-02-20 01:03:38 +0700

    Update til button2

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
-module.exports = router;
+router.get('/skills', function (req, res) {
+  let user_id = req.param('id')
+  let token = req.param('token')
+
+  res.send(user_id + ' ' + token)
+})
+
+router.get('/heroes', function (req, res) {
+  res.send(['Hulk', 'Spider-Man', 'Iron Man', 'Doctor Strange', 'Captain America', 'Deadpool', 'Thor', 'Ant-Man', 'Black Widow'])
+})
+
+module.exports = router
