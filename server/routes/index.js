2582c0408e69e3a51b01c41a3d83a48b8cea08f5
Fadly Kayo <fadly.kayo@googlemail.com>
2017-02-20 13:49:47 +0700

    Update index.html

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,13 +3,6 @@ var router = express.Router()
 
 /* GET home page. */
 
-router.get('/skills', function (req, res) {
-  let user_id = req.param('id')
-  let token = req.param('token')
-
-  res.send(user_id + ' ' + token)
-})
-
 router.get('/heroes', function (req, res) {
   res.send(['Hulk', 'Spider-Man', 'Iron Man', 'Doctor Strange', 'Captain America', 'Deadpool', 'Thor', 'Ant-Man', 'Black Widow'])
 })
