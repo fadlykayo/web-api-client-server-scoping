8fbd50c1ddfc425dfe57570a79d47f237ad596bb
Fadly Kayo <fadly.kayo@googlemail.com>
2017-02-20 18:19:56 +0700

    Update scope

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function (req, res, next) {
   res.send('index')
 })
 
+router.get('/heroes', function (req, res, next) {
+  res.send(['Hulk', 'Spider-Man', 'Iron Man', 'Doctor Strange', 'Captain America', 'Deadpool', 'Thor', 'Ant-Man', 'Black Widow'])
+})
+
 module.exports = router
