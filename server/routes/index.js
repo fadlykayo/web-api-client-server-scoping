8c1a402cd44b53cc38e17b0a7b6260f62f96afdd
Fadly Kayo <fadly.kayo@googlemail.com>
2017-02-20 15:14:55 +0700

    Done web api scope

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,9 +2,8 @@ var express = require('express')
 var router = express.Router()
 
 /* GET home page. */
-
-router.get('/heroes', function (req, res) {
-  res.send(['Hulk', 'Spider-Man', 'Iron Man', 'Doctor Strange', 'Captain America', 'Deadpool', 'Thor', 'Ant-Man', 'Black Widow'])
+router.get('/', function (req, res, next) {
+  res.send('index')
 })
 
 module.exports = router
